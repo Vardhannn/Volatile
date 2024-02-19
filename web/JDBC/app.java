b280824ddefcbc70bf627506f3215f733b9e20c4
vardhannn <iamsrivardhann@gmail.com>
2024-02-19 13:24:39 +0530

    added

---
+++ web/JDBC/app.java
@@ -0,0 +1,57 @@
+package JDBC;
+
+import java.sql.*;
+
+public class app {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/university";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    private static Connection connection;
+    private static Statement statement;
+    private static ResultSet resultSet;
+
+
+    public static void connect(){
+        try{
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void disconnect(){
+        try{
+            if (resultSet != null){
+                resultSet.close();
+            }
+            if (statement != null){
+                statement.close();
+            }
+            if (connection != null){
+                connection.close();
+            }
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        connect();
+        try{
+            statement = connection.createStatement();
+
+            String sql = "insert into student (RegNo,Name) values (211,var)";
+            statement.executeUpdate(sql);
+
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        finally{
+            disconnect();
+        }
+    }
+}
